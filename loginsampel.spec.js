4eaff7fc045581a646be5da9c9e93bc81c3960ef
komaldeshpande13 <76818003+komaldeshpande13@users.noreply.github.com>
2026-01-29 00:50:54 +0530

    Add files via upload
    test/loginsampel.spec.js

---
+++ loginsampel.spec.js
@@ -0,0 +1,23 @@
+ const {test, expect} = require('@playwright/test');
+ //const {LoginPage}=require('../pageobjects/LoginPage');
+ const {POManager}=require('../pageobjects/POManager');
+ const testdata=JSON.parse(JSON.stringify(require('../utils/testdata.json')));
+
+
+ for(const data of testdata){
+ test.only('@smoke Login as: '+data.role,async({page})=>{
+
+const pom=new POManager(page);
+const loginpage=pom.getLoginPage();
+await loginpage.goto();
+await loginpage.validate_login(data.username,data.password);
+
+await expect(page).toHaveTitle("Dashboard - The Man Project");
+
+await page.pause();
+
+
+
+
+ });
+}
