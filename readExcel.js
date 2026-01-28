3adf86329fb459fab3ee42b59778df526bee3f0b
komaldeshpande13 <76818003+komaldeshpande13@users.noreply.github.com>
2026-01-29 00:53:40 +0530

    Add files via upload

---
+++ readExcel.js
@@ -0,0 +1,10 @@
+const XLSX = require('xlsx');
+
+function readExcel(filePath, sheetName) {
+  const workbook = XLSX.readFile(filePath);
+  const sheet = workbook.Sheets[sheetName];
+  return XLSX.utils.sheet_to_json(sheet);
+}
+
+
+module.exports = { readExcel };
